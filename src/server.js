806e84986195c53733d5dd2c8345d27ce5460f4a
castilla-aurela <lfcastilla@gmail.com>
2021-08-10 20:39:07 -0500

    Structure

---
+++ src/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    usersApiUrl: process.env.USERS_API_URL || 'https://lcastilla-users.herokuapp.com/',
+    accountApiUrl: process.env.ACCOUNT_API_URL || 'https://lcastilla-accountsms.herokuapp.com/',
+    
+};
